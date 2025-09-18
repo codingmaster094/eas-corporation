1cf97d14fcb9a5c7b90054aa0dbe683647fb8abe
codingmaster094 <codingmaster094@gmail.com>
2025-09-18 12:19:32 +0530

    first commit

--- src/app/components/AnalyticsSection.js
+++ src/app/components/AnalyticsSection.js
@@ -13,34 +13,6 @@ export default function AnalyticsSection() {
             <span>{t("subtitle")}</span>
             <h2>{t("title")}</h2>
           </div>
-
-          <div className="analytics-text">
-            <p>{t("paragraph1")}</p>
-          </div>
-
-          <div className="analytics-text">
-            <p>{t("paragraph2")}</p>
-          </div>
-
-          <div className="analytics-text">
-            <p>{t("paragraph3")}</p>
-          </div>
-
-          <div className="analytics-text">
-            <p>{t("paragraph4")}</p>
-          </div>
-
-          <div className="analytics-text">
-            <p>{t("paragraph5")}</p>
-          </div>
-
-          <div className="analytics-text">
-            <p>{t("paragraph6")}</p>
-          </div>
-
-          <div className="analytics-text">
-            <p>{t("paragraph7")}</p>
-          </div>
         </div>
       </div>
     </section>
